29f227ec6e29a85de8703b147976c1eb9662c4c1
NikPeg <peganov.nik@gmail.com>
2025-03-11 23:30:56 +0300

    idk

--- precode.go
+++ precode.go
@@ -71,8 +71,13 @@ func main() {
 
 	var wg sync.WaitGroup
 
-	// 4. Собираем числа из каналов outs
-	// ...
+	// 4. Собираем числа из каналов outs ???
+	for ch := range outs {
+	    wg.Add(1)
+        go func(in <-chan int64, i int64){ amounts[i]++ }
+        wg.Done()
+	}
+    wg.WaitGroup()
 
 	go func() {
 		// ждём завершения работы всех горутин для outs
